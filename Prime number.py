e657e672df2094ef5f3b0b8e0c5b25952a087299
muntaha <muntahaiqbal@outlook.com>
2023-03-14 17:12:14 +0500

    2nd commit

--- Prime number.py
+++ Prime number.py
@@ -1,4 +1,5 @@
 def checkPrime(X):
+/*
   if X > 1:
     for i in range(2, int(X/2) + 1):
       if(X % i) == 0:
@@ -12,6 +13,7 @@ def checkPrime(X):
       print(X, " is a prime number")
   else:
     print(X, " is 1 or less than one and it's not prime")
+    */
 
 X = int(input("Enter a positive Number: "))
 checkPrime(X)
